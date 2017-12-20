60860c5fe25eebfe456564b5f000cdd1601117c2
goblinok <goblinok@gmail.com>
2017-12-20 15:05:57 +0900

    updated

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,7 @@ var eth = require('../dapp/eth.js');
 var express = require('express');
 var router = express.Router();
 
-//----------------------------
-//  펀딩 참여하기 입력화면
-//----------------------------
+/* GET users listing. */
 router.get('/new', function(req, res, next) {
 
     var fund_balance = eth.getBalance(constant.crowdFundContractAddress);
@@ -15,35 +13,28 @@ router.get('/new', function(req, res, next) {
     res.render('new', {fund : constant.crowdFundContractAddress, fund_balance : fund_balance, fund_tamount:fund_tamount});
 });
 
-//----------------------------
-//  메인화면, 사용자 상세화면
-//----------------------------
-router.get(['/', '/:user'], function(req, res, next) {
-    var user = req.params.user;
+router.get(['/', '/:id'], function(req, res, next) {
+    var id = req.params.id;
 
     var fund_balance = eth.getBalance(constant.crowdFundContractAddress);
     var fund_tamount = eth.getTokenAmount(constant.crowdFundContractAddress);  // token balanceof
 
-    if(user) {
-      // if user exists,
-      var user_balance = eth.getBalance(user);
-      var user_famount = eth.getFundAmount(user);  // fund balanceof
-      var user_tamount = eth.getTokenAmount(user);  // token balanceof
+    if(id) {
+        // if id exists,
+        var user_balance = eth.getBalance(id);
+        var user_famount = eth.getFundAmount(id);  // fund balanceof
+        var user_tamount = eth.getTokenAmount(id);  // token balanceof
 
-      res.render('view', {display:'',
-                          fund : constant.crowdFundContractAddress,fund_balance:fund_balance, fund_tamount:fund_tamount,
-                          user:user, user_balance : user_balance, user_famount:user_famount, user_tamount:user_tamount});
-    } else { 
-      // if user doesn't exist,
-      res.render('view', {display:'display:none',
-                          fund : constant.crowdFundContractAddress,fund_balance:fund_balance, fund_tamount:fund_tamount});
+        res.render('view', {display:'',
+            fund : constant.crowdFundContractAddress,fund_balance:fund_balance, fund_tamount:fund_tamount,
+            user:id, user_balance : user_balance, user_famount:user_famount, user_tamount:user_tamount});
+    } else {
+        // if id doesn't exist,
+        res.render('view', {display:'display:none',
+            fund : constant.crowdFundContractAddress,fund_balance:fund_balance, fund_tamount:fund_tamount});
     }
 });
 
-
-//----------------------------
-//  펀딩 참여하기 액션
-//----------------------------
 router.post('/join', function(req, res, next) {
     var from = req.body.user;
     var famount = req.body.famount;
@@ -56,7 +47,7 @@ router.post('/join', function(req, res, next) {
             console.log(err);
             return callback(err);
         } else {
-            eth.sendTransaction(from, constant.crowdFundContractAddress, famount, 100000, checkTransaction);
+            eth.sendTransaction(from, constant.crowdFundContractAddress, famount, checkTransaction);
         }
     }
 
